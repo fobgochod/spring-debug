a9a4c1aa7f2d0735934856988bb3b4f3889b6336
zhouxiao <764800230@qq.com>
2021-04-07 01:10:36 +0800

    lesson5

---
+++ src/main/java/com/mashibing/lesson5/Lesson5.java
@@ -0,0 +1,14 @@
+package com.mashibing.lesson5;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Lesson5 {
+
+    public static void main(String[] args) {
+
+        ApplicationContext ac = new ClassPathXmlApplicationContext("classpath:lesson5/lesson5.xml");
+
+
+    }
+}
---
+++ src/main/java/com/mashibing/lesson5/README.md
@@ -0,0 +1,10 @@
+# Lesson5
+
+initBeanDefinitionReader(beanDefinitionReader); 适配器模式？
+
+
+ClassPathXmlApplicationContext.java:142
+setConfigLocations(configLocations);
+
+AbstractXmlApplicationContext.java:127
+String[] configLocations = getConfigLocations();
