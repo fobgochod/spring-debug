9ca3c64102e5f49fb67463484932a01057b6ff71
zhouxiao <764800230@qq.com>
2021-08-02 18:33:32 +0800

    update

--- spring-boot/src/main/java/com/fobgochod/autoconfigure/MyAutoConfiguration.java
+++ spring-boot/src/main/java/com/fobgochod/autoconfigure/MyAutoConfiguration.java
@@ -1,7 +1,14 @@
 package com.fobgochod.autoconfigure;
 
+import com.fobgochod.domain.User;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class MyAutoConfiguration {
+
+    @Bean
+    public User user() {
+        return new User();
+    }
 }
